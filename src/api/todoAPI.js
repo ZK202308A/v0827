b1d6caf28acb6d28f8350a7aebca6abc8c246936
zk202308A <zk202308A@gmail.com>
2024-08-27 14:39:46 +0900

    paging

--- src/api/todoAPI.js
+++ src/api/todoAPI.js
@@ -3,9 +3,9 @@ import axios from "axios"
 
 const host = 'http://localhost:8088/api/v1/todos'
 
-export const getList = async () => {
+export const getList = async (pageNum) => {
 
-  const res = await axios.get(`${host}/list`)
+  const res = await axios.get(`${host}/list`, {params : {page: pageNum || 1}})
 
   return res.data
 }
--- src/components/todo/ListComponent.vue
+++ src/components/todo/ListComponent.vue
@@ -9,25 +9,102 @@
           </RouterLink>
         </p>
     </div>
+
+    <div class="d-flex justify-content-center align-items-center" style="height: 200px;">
+      <ul class="pagination">
+        <li :class="`page-item ${ page == serverData.number+1 ?'active':''}`" v-for="{page,label} in pageNums" :key="page">
+          <a class="page-link" @click="() => handleClickPage(page)" >{{label}}</a>
+        </li>
+      </ul>
+    </div>
+
 </template>
 
 <script setup>
 
-import { onMounted , ref} from 'vue';
+import { computed, onMounted , ref} from 'vue';
 import { getList } from '../../api/todoAPI';
-import { RouterLink } from 'vue-router';
+import { onBeforeRouteUpdate, RouterLink, useRoute, useRouter } from 'vue-router';
 
 const serverData = ref({
-  content: []
+  content: [],
+  number: 0,
+  size: 0,
+  totalPages:0
 })
+
+const router = useRouter()
+const route = useRoute()
+
+const handleClickPage = (pageNum) => {
+
+  const currentQueryPage = parseInt(route.query.page || 1)
+
+  console.log('handleClickPage ' + pageNum + " : " + route.query.page)
+
+  if(currentQueryPage == pageNum) {
+    console.log("동일 페이지 클릭")
+
+    getList(pageNum).then(res => serverData.value = res)
+
+  }else {
+    console.log("다른 페이지 클릭")
+    router.push({path:'/todo/list', query: {page:pageNum}})
+  }
+}
+
+const pageNums = computed(() => {
+
+  const current = serverData.value.number + 1
+
+  let lastPageNum = Math.ceil(current/10.0) * 10
+
+  const startPageNum = lastPageNum - 9 
+  const prev = startPageNum !== 1
+  let next = true
+
+  if(serverData.value.totalPages <= lastPageNum) {
+    lastPageNum = serverData.value.totalPages
+    next = false
+  }
+
+  //arr
+  const arr = []
+  if(prev){
+    arr.push({page: startPageNum-1, label:"Prev"})
+  }
+  for(let i = startPageNum; i <= lastPageNum; i++){
+    arr.push({page: i, label: i})
+  }
+  if(next){
+    arr.push({page: lastPageNum + 1 , label:"Next"})
+  }
+  return arr
+})
+
 
 
 onMounted(async () => {
   
-  const result = await getList()
+  const page = route.query.page || 1
+
+  const result = await getList(page)
 
   serverData.value = result
 
+  console.log(serverData.value)
+
+})
+
+onBeforeRouteUpdate(async (to,from,next) => {
+
+  console.log('onBeforeRouteUpdate')
+
+  const result = await getList(to.query.page)
+
+  serverData.value = result
+
+  next()  
 })
 
 
